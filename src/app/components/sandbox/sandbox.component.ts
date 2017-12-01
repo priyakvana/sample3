9a6a706ec729f7cb01639e2af4f07cd8413fee0e
priyanka <priya.kvana@gmail.com>
2017-12-01 18:40:10 +0530

    sample website

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,22 @@
+import { Component }from'@angular/core';
+
+@Component({
+    selector:'sandbox',
+    template:
+   ` <h1> Hello </h1>
+   <div><img src="{{ imageUrl1 }}"> </div>
+   <div><img [src]=" imageUrl2"> </div>
+   <div><img bind-src="imageUrl3"> </div>
+   <div><img src="{{ imageUrl }}"> </div>
+   <hr>
+   <h2>create post</h2>
+   <p [hidden]="isUnchanged">post has been changed,please save</p>
+   <button [disabled]="isUnchanged">save</button>
+   `
+})
+export class SandboxComponent{
+  imageUrl1='assets/images/image22.jpg';
+  imageUrl2='assets/images/image33.jpg';
+  imageUrl3='assets/images/image44.jpg'
+  isUnchanged:boolean=false;
+}
