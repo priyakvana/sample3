696a3ecc0987144c79d39e352a01ae4df4480e15
priyanka <priya.kvana@gmail.com>
2017-12-04 18:45:05 +0530

    added forms

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,22 +2,19 @@ import { Component }from'@angular/core';
 
 @Component({
     selector:'sandbox',
-    template:
-   ` <h1> Hello </h1>
-   <div><img src="{{ imageUrl1 }}"> </div>
-   <div><img [src]=" imageUrl2"> </div>
-   <div><img bind-src="imageUrl3"> </div>
-   <div><img src="{{ imageUrl }}"> </div>
-   <hr>
-   <h2>create post</h2>
-   <p [hidden]="isUnchanged">post has been changed,please save</p>
-   <button [disabled]="isUnchanged">save</button>
-   
-   `
+    template:`
+    <h1>Hello world</h1>
+   <input type="text"(click)="fireEvent($event)" placeholder="click" value={{text}} (keypress)="changeEvent($event)">
+   <h2>{{text}}</h2>
+   ` 
 })
 export class SandboxComponent{
-  imageUrl1='assets/images/image22.jpg';
-  imageUrl2='assets/images/image33.jpg';
-  imageUrl3='assets/images/image44.jpg'
-  isUnchanged:boolean=false;
+  text:string='hello';
+fireEvent(e){
+  console.log(e.type);
+  }
+  changeEvent(e){
+    this.text= e.target.value;
+  }
+
 }
