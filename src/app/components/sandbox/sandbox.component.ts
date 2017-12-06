03d1d1a5fe745476fd9eaaf379c77a8cc648a925
priyanka <priya.kvana@gmail.com>
2017-12-06 15:05:11 +0530

    working on http POST Request call

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -4,27 +4,45 @@ import { DataServices } from '../../services/data.services';
 @Component({
     selector:'sandbox',
     template:`
+  <form (submit)="onSubmit()">
+    <label>Name</label>
+    <input type="text" class="form-control" [(ngModel)]="user.name" name="name">
+    <label>E-mail</label>
+    <input type="text" class="form-control" [(ngModel)]="user.email" name="email">
+    <label>Phone No</label>
+    <input type="text" class="form-control" [(ngModel)]="user.phone" name="Phone No">
+    <input type="submit" class="btn btn-success" value="submit">
+    </form>
     <div *ngFor="let user of users">
     <div class="container jumbotron" style="background:pink; !important">
-    <ul class="list-group">
-    <li >Name:{{user.name}}</li>
-    <li >Email:{{user.email}}</li>
-    <li >Phone No:{{user.phone}}</li>
-    </ul>
+          <ul class="list-group">
+             <li >Name:{{user.name}}</li>
+              <li >Email:{{user.email}}</li>
+              <li >Phone No:{{user.phone}}</li>
+            </ul>
     <br>
     </div>
     </div>
    `  
 })
 export class SandboxComponent{
-users:any[]= [];
+users:any[];
+user={
+   name:'',
+   email:'',
+   phone:''
+}
 constructor(public dataService:DataServices)
 {
   this.dataService.getUsers().subscribe(users=>{
     //console.log(users);
     this.users=users;
   });
- 
 }
-  
+  onSubmit(){
+     this.dataService.addUser(this.user).subscribe(user => {
+       console.log(user);
+       this.users.unshift(user);
+     });
+ }  
 }
--- src/app/services/data.services.ts
+++ src/app/services/data.services.ts
@@ -12,5 +12,9 @@ export class DataServices{
         return this.http.get('http://jsonplaceholder.typicode.com/users')
        .map(res=>res.json());
     }
+    addUser(user){
+        return this.http.post('http://jsonplaceholder.typicode.com/users',user)
+        .map(res=>res.json()); 
+    }
    
     } 
